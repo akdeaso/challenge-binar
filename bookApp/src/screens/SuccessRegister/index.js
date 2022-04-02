00971efa39fca9fdfa8c435a28644a3155a67f38
akdeaso <akdeaso@gmail.com>
2022-04-02 12:41:05 +0700

    add splash screen and some screen

---
+++ bookApp/src/screens/SuccessRegister/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SuccessRegister = () => {
+    return (
+        <View>
+            <Text>SuccessRegister</Text>
+        </View>
+    )
+}
+
+export default SuccessRegister
+
+const styles = StyleSheet.create({})
